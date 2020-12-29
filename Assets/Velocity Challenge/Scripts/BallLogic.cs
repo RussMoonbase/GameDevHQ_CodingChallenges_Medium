637095c104ec4fee2d7b3bf51409a85ae27be166
Ian <russmoonbase@gmail.com>
2020-12-29 12:38:39 -0700

    First commit after finishing velocity challenge

---
+++ Assets/Velocity Challenge/Scripts/BallLogic.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallLogic : MonoBehaviour
+{
+   private Rigidbody _rigidBody;
+   float _ballVelocity;
+   [SerializeField] float _velocityIncrease;
+
+   // Start is called before the first frame update
+   void Start()
+   {
+      _rigidBody = GetComponent<Rigidbody>();
+      _ballVelocity = _velocityIncrease;
+   }
+
+   // Update is called once per frame
+   void Update()
+   {
+
+   }
+
+   private void OnCollisionEnter(Collision other)
+   {
+      if (other.gameObject.tag == "Floor")
+      {
+         _ballVelocity += _velocityIncrease;
+         _rigidBody.velocity = new Vector3(0f, _ballVelocity, 0f);
+      }
+   }
+}
