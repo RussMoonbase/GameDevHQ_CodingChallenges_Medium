35283d41edfb1f4825934f27a301cbe5033245ff
Ian <russmoonbase@gmail.com>
2020-12-29 14:24:50 -0700

    Added falling challenge

---
+++ Assets/_Challenges/Falling Forever Challenge/Scripts/FallingForever_Spawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingForever_Spawner : MonoBehaviour
+{
+   private static FallingForever_Spawner _instance;
+   public static FallingForever_Spawner Instance
+   {
+      get
+      {
+         if (!_instance)
+         {
+            Debug.Log("Spawner didn't load");
+         }
+
+         return _instance;
+      }
+   }
+
+   [SerializeField] private GameObject _theBall;
+
+   private void Awake()
+   {
+      _instance = this;
+   }
+}
