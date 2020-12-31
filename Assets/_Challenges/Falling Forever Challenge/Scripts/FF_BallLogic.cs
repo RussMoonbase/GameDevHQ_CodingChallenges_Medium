33032f95bbedfe0318e6d9e72a6f4c7d150d60a5
Ian <russmoonbase@gmail.com>
2020-12-31 13:45:39 -0700

    Finished Falling Forever Challenge

---
+++ Assets/_Challenges/Falling Forever Challenge/Scripts/FF_BallLogic.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FF_BallLogic : MonoBehaviour
+{
+   [SerializeField] Renderer _renderer;
+
+   private void OnCollisionEnter(Collision other)
+   {
+      if (other.gameObject.tag == "Floor")
+      {
+         Hide();
+      }
+   }
+
+   private void Hide()
+   {
+      this.gameObject.SetActive(false);
+      FallingForever_Spawner.Instance.ResetBall(this.gameObject.transform, _renderer);
+   }
+}
--- Assets/_Challenges/Falling Forever Challenge/Scripts/FallingForever_Spawner.cs
+++ Assets/_Challenges/Falling Forever Challenge/Scripts/FallingForever_Spawner.cs
@@ -18,10 +18,72 @@ public class FallingForever_Spawner : MonoBehaviour
       }
    }
 
-   [SerializeField] private GameObject _theBall;
+   private Renderer _renderer;
+
+   [SerializeField] private GameObject _ballPrefab;
+   [SerializeField] private List<GameObject> _ballPool;
+   [SerializeField] private int _ballSpawnAmount = 1;
+   [SerializeField] private Vector3 _startPosition;
 
    private void Awake()
    {
       _instance = this;
    }
+
+   private void Start()
+   {
+      _ballPool = GenerateBalls(_ballSpawnAmount);
+      _renderer = GetComponent<Renderer>();
+   }
+
+   private List<GameObject> GenerateBalls(int ballAmount)
+   {
+      for (int i = 0; i < ballAmount; i++)
+      {
+         GameObject ball = Instantiate(_ballPrefab);
+         _ballPool.Add(ball);
+      }
+
+      return _ballPool;
+   }
+
+   public void ResetBall(Transform ballTransform, Renderer ballRenderer)
+   {
+      ResetBallToStartPosition(ballTransform);
+      AssignRandomColorToBall(ballRenderer);
+
+      foreach (var aBall in _ballPool)
+      {
+         if (!aBall.activeInHierarchy)
+         {
+            aBall.SetActive(true);
+            break;
+         }
+      }
+   }
+
+   private void ResetBallToStartPosition(Transform theBall)
+   {
+      theBall.position = _startPosition;
+   }
+
+   private void AssignRandomColorToBall(Renderer theBallRenderer)
+   {
+      //Random.seed = System.DateTime.Now.Millisecond;
+      if (theBallRenderer)
+      {
+         theBallRenderer.material.color = new Color(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f));
+      }
+   }
+
+   //public GameObject RequestBall()
+   //{
+   //   foreach (var aBall in _ballPool)
+   //   {
+   //      if (!aBall.activeInHierarchy)
+   //      {
+
+   //      }
+   //   }
+   //}
 }
